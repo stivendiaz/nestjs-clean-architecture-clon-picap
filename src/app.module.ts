f51984796f4031a9626bc67ae0dc762763ab7b13
stivendiaz <stivendiazagudelo@gmail.com>
2023-02-15 18:21:37 -0500

    Add openapi specification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DriverController } from './driver/driver.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DriverController],
   providers: [AppService],
 })
 export class AppModule {}
